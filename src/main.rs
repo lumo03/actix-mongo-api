fd79a83c5e540baa59ec7069c5f8814f5c4dd832
lumo03 <luis.moncada.mso@gmail.com>
2023-01-20 17:06:54 +0100

    v. 0.1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, Responder, HttpResponse, HttpServer, App};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().json("Hello from Rust and MongoDB.")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+        .service(hello)
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await
 }
