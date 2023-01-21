da612631b50756a2ba570e9bea5c480f929f5909
lumo03 <luis.moncada.mso@gmail.com>
2023-01-21 19:39:09 +0100

    v 1.0

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use actix_web::{get, Responder, HttpResponse, HttpServer, App};
+use actix_web::{get, web::Data, App, HttpResponse, HttpServer, Responder};
+use api::user_api::{create_user, delete_user, get_all_users, get_user, update_user};
+use repository::mongodb_repo::MongoRepo;
+
+mod api;
+mod models;
+mod repository;
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -7,11 +13,18 @@ async fn hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let db = MongoRepo::init().await;
+    let db_data = Data::new(db);
+    HttpServer::new(move || {
         App::new()
-        .service(hello)
+            .app_data(db_data.clone())
+            .service(create_user)
+            .service(get_user)
+            .service(update_user)
+            .service(delete_user)
+            .service(get_all_users)
     })
-    .bind("0.0.0.0:8080")?
+    .bind("127.0.0.1:8080")?
     .run()
     .await
 }
